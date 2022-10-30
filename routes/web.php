05b2b1b72fb4eb7dfc690b33e24d89ac486ded42
omar algalfy <algalfy71@gmail.com>
2022-10-30 15:45:52 +0200

    admin middelware and testing it

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+// use App\Http\Controllers\AdminController;
+
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +24,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/',[AdminController::class,'index'])->name('index');
+});
 
 require __DIR__.'/auth.php';
