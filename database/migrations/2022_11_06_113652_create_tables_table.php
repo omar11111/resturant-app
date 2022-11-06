46af7c23335b227cfec636495cce22c283e6b58a
omar algalfy <algalfy71@gmail.com>
2022-11-06 18:02:44 +0200

    models and migrations

---
+++ database/migrations/2022_11_06_113652_create_tables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('guest_number');
+            $table->string('description');
+            $table->string('location');
+            $table->string('status')->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
