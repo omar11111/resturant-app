36fb5968a7c32d662af441f974187ff6c9306c5f
omar algalfy <algalfy71@gmail.com>
2022-11-09 17:43:00 +0200

    install mcamara localiztion package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Admin;
+use App\Http\Middleware\Localization;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,6 +39,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            Localization::class,
         ],
 
         'api' => [
@@ -64,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin'=> Admin::class
+        'admin'=> Admin::class,
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 // use App\Http\Controllers\AdminController;
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\MenuController;
-use App\Http\Controllers\Admin\ReservationController;
-use App\Http\Controllers\Admin\TableController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ReservationController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 /*
@@ -20,21 +21,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
+        Route::get('/',[AdminController::class,'index'])->name('index');
+        Route::resource('/menus',MenuController::class);
+        Route::resource('/categories',CategoryController::class);
+        Route::resource('/reservations',ReservationController::class);
+        Route::resource('/tables',TableController::class);
+    });
 
-Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
-    Route::get('/',[AdminController::class,'index'])->name('index');
-    Route::resource('/menus',MenuController::class);
-    Route::resource('/categories',CategoryController::class);
-    Route::resource('/reservations',ReservationController::class);
-    Route::resource('/tables',TableController::class);
 });
 
 require __DIR__.'/auth.php';
